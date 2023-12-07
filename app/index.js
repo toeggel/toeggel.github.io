69d0ff808f12d7dceb78f5fc95daf53c96da1ece
Martin Weber <weber_martin@gmx.ch>
2023-12-07 14:02:50 +0100

    remove everything

--- app/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./environment"));
-__export(require("./app.component"));
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNsaWVudC9hcHAvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlCQUFjLGVBQWUsQ0FBQyxFQUFBO0FBQzlCLGlCQUFjLGlCQUFpQixDQUFDLEVBQUEiLCJmaWxlIjoiY2xpZW50L2FwcC9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gXCIuL2Vudmlyb25tZW50XCI7XG5leHBvcnQgKiBmcm9tIFwiLi9hcHAuY29tcG9uZW50XCI7XG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
